3eaa3590ecc4b8f20f1f5210dc50547f5272fbd0
sptkl <caobaiyue@gmail.com>
2019-09-20 16:31:54 +0000

    completing fill script

--- 03_build.sh
+++ 03_build.sh
@@ -1,4 +1,4 @@
 DB_CONTAINER_NAME=melissa
 
-# docker exec $DB_CONTAINER_NAME psql -U postgres -h localhost -f sql/create.sql
+docker exec $DB_CONTAINER_NAME psql -U postgres -h localhost -f sql/create.sql
 docker exec $DB_CONTAINER_NAME psql -U postgres -h localhost -f sql/fill.sql
--- sql/fill.sql
+++ sql/fill.sql
@@ -21,7 +21,7 @@ SET borough_code = b.e_wa1_street1_boroughcode,
     blockfaceid = b.e_wa2_blockfaceid, 
     cd = b.e_wa2_communitydistrict, 
     nta = b.e_wa2_nta,
-    -- nta_name = 
+    nta_name = b.e_wa2_ntaname,
     f1_grc = b.e_wa2_grc, 
     f1_reasoncode = b.e_wa2_reasoncode,
     f1_message = b.e_wa1_message,
@@ -41,5 +41,64 @@ SET borough_code = b.e_wa1_street1_boroughcode,
     fap_grc = b.ap_wa2_grc,
     fap_reasoncode = b.ap_wa2_reasoncode,
     fap_message = b.ap_wa1_message
-FROM melissa_input_geocode b
+FROM melissa_corrections_geocode AS b 
+WHERE melissa.id = b.id;
+
+UPDATE melissa
+SET borough_code = (CASE WHEN borough_code IS NULL THEN b.e_wa1_street1_boroughcode ELSE borough_code END),
+    f1_normalized_hn = (CASE WHEN f1_normalized_hn IS NULL THEN b.e_wa1_housenumberdisplay ELSE f1_normalized_hn END),
+    f1_normalized_sn = (CASE WHEN f1_normalized_sn IS NULL THEN b.e_wa1_street1_streetname ELSE f1_normalized_sn END),
+    centerline_xcoordinate = (CASE WHEN centerline_xcoordinate IS NULL THEN b.e_wa2_xcoordinate ELSE centerline_xcoordinate END), 
+    centerline_ycoordinate = (CASE WHEN centerline_ycoordinate IS NULL THEN b.e_wa2_ycoordinate ELSE centerline_ycoordinate END),
+    centerline_latitude = (CASE WHEN centerline_latitude IS NULL THEN b.e_wa2_latitude ELSE centerline_latitude END), 
+    centerline_longitude = (CASE WHEN centerline_longitude IS NULL THEN b.e_wa2_longitude ELSE centerline_longitude END),
+    physicalid = (CASE WHEN physicalid IS NULL THEN b.e_wa2_physicalid ELSE physicalid END),
+    blockfaceid = (CASE WHEN blockfaceid IS NULL THEN b.e_wa2_blockfaceid ELSE blockfaceid END),
+    cd = (CASE WHEN cd IS NULL THEN b.e_wa2_communitydistrict ELSE cd END),
+    nta = (CASE WHEN nta IS NULL THEN b.e_wa2_nta ELSE nta END),
+    nta_name = (CASE WHEN nta_name IS NULL THEN b.e_wa2_ntaname ELSE nta_name END),
+    f1_grc = (CASE WHEN f1_grc IS NULL THEN b.e_wa2_grc ELSE f1_grc END),
+    f1_reasoncode = (CASE WHEN f1_reasoncode IS NULL THEN b.e_wa2_reasoncode ELSE f1_reasoncode END),
+    f1_message = (CASE WHEN f1_message IS NULL THEN b.e_wa1_message ELSE f1_message END),
+    f1a_normalized_hn = (CASE WHEN f1a_normalized_hn IS NULL THEN b.a_wa1_housenumberdisplay ELSE f1a_normalized_hn END),
+    f1a_normalized_sn = (CASE WHEN f1a_normalized_sn IS NULL THEN b.a_wa1_street1_streetname ELSE f1a_normalized_sn END),
+    bbl = (CASE WHEN bbl IS NULL THEN b.a_wa2_bbl ELSE bbl END),
+    f1a_grc = (CASE WHEN f1a_grc IS NULL THEN b.a_wa2_grc ELSE f1a_grc END),
+    f1a_reasoncode = (CASE WHEN f1a_reasoncode IS NULL THEN b.a_wa2_reasoncode ELSE f1a_reasoncode END),
+    f1a_message = (CASE WHEN f1a_message IS NULL THEN b.a_wa1_message ELSE f1a_message END),
+    fap_normalized_hn = (CASE WHEN fap_normalized_hn IS NULL THEN b.ap_wa1_housenumberdisplay ELSE fap_normalized_hn END),
+    fap_normalized_sn = (CASE WHEN fap_normalized_sn IS NULL THEN b.ap_wa1_street1_streetname ELSE fap_normalized_sn END),
+    addresspointid = (CASE WHEN addresspointid IS NULL THEN b.ap_wa2_ap_id ELSE addresspointid END),
+    addresspointid_xcoordinate = (CASE WHEN addresspointid_xcoordinate IS NULL THEN left(b.ap_wa2_xcoordinate, 7) ELSE addresspointid_xcoordinate END),
+    addresspointid_ycoordinate = (CASE WHEN addresspointid_xcoordinate IS NULL THEN right(b.ap_wa2_ycoordinate,7) ELSE addresspointid_xcoordinate END),
+    addresspointid_latitude = (CASE WHEN addresspointid_latitude IS NULL THEN b.ap_wa2_latitude ELSE addresspointid_latitude END),
+    addresspointid_longitude = (CASE WHEN addresspointid_longitude IS NULL THEN b.ap_wa2_longitude ELSE addresspointid_longitude END),
+    fap_grc = (CASE WHEN fap_grc IS NULL THEN b.ap_wa2_grc ELSE fap_grc END),
+    fap_reasoncode = (CASE WHEN fap_reasoncode IS NULL THEN b.ap_wa2_reasoncode ELSE fap_reasoncode END),
+    fap_message = (CASE WHEN fap_message IS NULL THEN b.ap_wa1_message ELSE fap_message END)
+FROM melissa_input_geocode AS b 
+WHERE melissa.id = b.id;
+
+UPDATE melissa
+SET bin = (CASE 
+            WHEN b.a_wa2_tpadnewbin IS NOT NULL THEN  b.a_wa2_tpadnewbin
+            ELSE b.a_wa2_binofinputaddress
+        END),
+    is_tpad_bin = (CASE 
+            WHEN b.a_wa2_tpadnewbin IS NOT NULL THEN 'Y'
+            ELSE 'N'
+        END)
+FROM melissa_corrections_geocode AS b 
+WHERE melissa.id = b.id;
+
+UPDATE melissa
+SET bin = (CASE 
+            WHEN bin IS NULL AND b.a_wa2_tpadnewbin IS NOT NULL THEN b.a_wa2_tpadnewbin
+            ELSE b.a_wa2_binofinputaddress
+        END),
+    is_tpad_bin = (CASE 
+            WHEN is_tpad_bin IS NULL AND b.a_wa2_tpadnewbin IS NOT NULL THEN 'Y'
+            ELSE 'N'
+        END)
+FROM melissa_input_geocode AS b 
 WHERE melissa.id = b.id;
